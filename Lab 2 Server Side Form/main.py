8fa1be939727fc932076a0cc234bcc24ca066bee
ericgrogers <ericgrogers@fullsail.edu>
2014-08-12 15:13:20 -0500

    Added head section for the Page() class

--- Lab 2 Server Side Form/main.py
+++ Lab 2 Server Side Form/main.py
@@ -11,7 +11,7 @@ import webapp2
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         self.response.write('Hello world!')
-        
+
 
 #Create a Page class to hold all of the view model templates.
 class Page(object):
@@ -22,6 +22,28 @@ class Page(object):
 
         #set the location of the CSS file.
         self.css = "css/styles.css"
+
+         #create the head section of the page.
+        self.head = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>{self.title}</title>
+    <link rel="stylesheet" type="text/css" href={self.css}>
+</head>
+<body>
+<header>
+    <h1>Eric Rogers</h1>
+    <h2>designer/developer</h2>
+    <nav>
+        <ul>
+            <li><a href="#">pricing</a></li>
+            <li><a href="#">portfolio</a></li>
+            <li><a class="active_link" href="#">contact</a></li>
+        </ul>
+    </nav>
+</header>'''
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
