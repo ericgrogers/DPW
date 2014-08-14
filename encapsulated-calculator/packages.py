9ca109d9fc3d9c9eb3ec0a0c20ba922a238eae8c
ericgrogers <ericgrogers@fullsail.edu>
2014-08-14 16:39:43 -0500

    creating getters and setters for the MaintenancePackage

--- encapsulated-calculator/packages.py
+++ encapsulated-calculator/packages.py
@@ -19,3 +19,32 @@ class MaintenancePackage(object):
         self.__discount_rate = 0
         self.__total = 0
         self.__discounted_total = 0
+
+    #getters and setters for each attribute(property)
+    @property
+    def package_name(self):
+        return self.__package_name
+
+    @package_name.setter
+    def package_name(self, new_package_name):
+        self.__package_name = new_package_name
+
+    @property
+    def expires(self):
+        return self.__expires
+
+    @expires.setter
+    def expires(self, new_expires):
+        self.__expires = new_expires
+
+    @property
+    def hours(self):
+        return self.__hours
+
+    @hours.setter
+    def hours(self, new_hours):
+        self.__hours = new_hours
+
+    @property
+    def rate(self):
+        return self.__rate
