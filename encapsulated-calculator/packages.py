634178eaabcdc0399824506453d6186b1bc01f60
ericgrogers <ericgrogers@fullsail.edu>
2014-08-14 16:38:12 -0500

    set up private attributes for the packages

--- encapsulated-calculator/packages.py
+++ encapsulated-calculator/packages.py
@@ -6,6 +6,16 @@ Encapsulated Calculator
 """
 
 
+#class for the maintenance packages.
 class MaintenancePackage(object):
     def __init__(self):
-        self.package_name = ''
+
+        #making attributes private
+        self.__package_name = ''
+        self.__expires = ''
+        self.__hours = 0
+        self.__rate = 0
+        self.__discount = 0.1
+        self.__discount_rate = 0
+        self.__total = 0
+        self.__discounted_total = 0
