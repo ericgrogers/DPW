596c789953d492abe480cf2eebd53da82243b230
ericgrogers <ericgrogers@fullsail.edu>
2014-08-14 16:36:06 -0500

    setting up classes for the app.

---
+++ encapsulated-calculator/packages.py
@@ -0,0 +1,11 @@
+"""
+Eric Rogers
+08/14/2014
+Design Patterns for Web Programming
+Encapsulated Calculator
+"""
+
+
+class MaintenancePackage(object):
+    def __init__(self):
+        self.package_name = ''
---
+++ encapsulated-calculator/pages.py
@@ -0,0 +1,13 @@
+"""
+Eric Rogers
+08/14/2014
+Design Patterns for Web Programming
+Encapsulated Calculator
+"""
+
+
+class Page(object):
+    def __init__(self):
+        self.head = ''''''
+        self.body = ''''''
+        self.close = ''''''
