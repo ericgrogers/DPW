6749b38930f4c5fa29b146aea1d22124e0ff4896
ericgrogers <ericgrogers@fullsail.edu>
2014-08-14 17:15:12 -0500

    created default attributes for the Page() class

--- encapsulated-calculator/pages.py
+++ encapsulated-calculator/pages.py
@@ -6,8 +6,27 @@ Encapsulated Calculator
 """
 
 
+#class for all of the page html markup
 class Page(object):
     def __init__(self):
+
+        #set the default page attributes
+        self.title = 'Pricing'
+        self.css = 'css/styles.css'
+        self.package_name = 'Click on a button to view package details.'
+        self.__expires = ''
+        self.__hours = ''
+        self.__rate = ''
+        self.__discount_rate = ''
+        self.__total = ''
+        self.__discounted_total = ''
+        self.expires_label = ''
+        self.hours_label = ''
+        self.rate_label = ''
+        self.discount_rate_label = ''
+        self.total_label = ''
+        self.discounted_total_label = ''
+
         self.head = ''''''
         self.body = ''''''
         self.close = ''''''
