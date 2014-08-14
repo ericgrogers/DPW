3f92db8443cd1152181bbd4a574c72dbe8c7b92a
ericgrogers <ericgrogers@fullsail.edu>
2014-08-14 17:13:27 -0500

    finishing getters and setter for the maintenance packages

--- encapsulated-calculator/packages.py
+++ encapsulated-calculator/packages.py
@@ -48,3 +48,39 @@ class MaintenancePackage(object):
     @property
     def rate(self):
         return self.__rate
+
+    @rate.setter
+    def rate(self, new_rate):
+        self.__rate = new_rate
+
+    @property
+    def discount(self):
+        return self.__discount
+
+    @discount.setter
+    def discount(self, new_discount):
+        self.__discount = new_discount
+
+    @property
+    def discount_rate(self):
+        return self.__discount_rate
+
+    @discount_rate.setter
+    def discount_rate(self, new_discount_rate):
+        self.__discount_rate = new_discount_rate
+
+    @property
+    def total(self):
+        return self.__total
+
+    @total.setter
+    def total(self, new_total):
+        self.__total = new_total
+
+    @property
+    def discounted_total(self):
+        return self.__discounted_total
+
+    @discounted_total.setter
+    def discounted_total(self, new_discounted_total):
+        self.__discounted_total = new_discounted_total
