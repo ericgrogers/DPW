02a4781e3efee1be62f60ab6cc84fe6a16278a99
ericgrogers <ericgrogers@fullsail.edu>
2014-08-14 19:56:18 -0500

    added getters and setters for the Page class

--- encapsulated-calculator/pages.py
+++ encapsulated-calculator/pages.py
@@ -119,3 +119,52 @@ class Page(object):
         self.discount_rate_label = "Discount Rate:"
         self.total_label = "Total:"
         self.discounted_total_label = "Discounted:"
+
+    #getters and setters for the page attributes(properties)
+    @property
+    def expires(self):
+        return self.__expires
+
+    @expires.setter
+    def expires(self, new_expires):
+        self.__expires = new_expires
+
+    @property
+    def hours(self):
+        return self.__hours
+
+    @hours.setter
+    def hours(self, new_hours):
+        self.__hours = new_hours
+
+    @property
+    def rate(self):
+        return self.__rate
+
+    @rate.setter
+    def rate(self, new_rate):
+        self.__rate = new_rate
+
+    @property
+    def discount_rate(self):
+        return self.__discount_rate
+
+    @discount_rate.setter
+    def discount_rate(self, new_discount_rate):
+        self.__discount_rate = new_discount_rate
+
+    @property
+    def total(self):
+        return self.__total
+
+    @total.setter
+    def total(self, new_total):
+        self.__total = new_total
+
+    @property
+    def discounted_total(self):
+        return self.__discounted_total
+
+    @discounted_total.setter
+    def discounted_total(self, new_discounted_total):
+        self.__discounted_total = new_discounted_total
