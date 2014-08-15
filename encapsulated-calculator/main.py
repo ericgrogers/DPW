9343beab54388d690a5f6c6c8d75e1d0df286e36
ericgrogers <ericgrogers@fullsail.edu>
2014-08-14 20:09:19 -0500

    placed the packages in an array,
    created a method to select packages

--- encapsulated-calculator/main.py
+++ encapsulated-calculator/main.py
@@ -32,6 +32,24 @@ class MainHandler(webapp2.RequestHandler):
             #return a formatted total, removing decimal places and adding a comma in the proper places.
             return "{:,.0f}".format(total)
 
+        #select the appropriate package using GET when the user clicks on one of the links.
+        def select_package():
+
+            #set pack to the package name
+            pack = self.request.GET['package']
+
+            #check each package name for a match and call build_package() using the index location of the package within pkgs.
+            if pack == 'basic':
+                build_package(0)
+            elif pack == 'bronze':
+                build_package(1)
+            elif pack == 'silver':
+                build_package(2)
+            elif pack == 'gold':
+                build_package(3)
+            elif pack == 'platinum':
+                build_package(4)
+
         #basic package object
         basic = MaintenancePackage()
         basic.package_name = "Basic"
@@ -42,6 +60,9 @@ class MainHandler(webapp2.RequestHandler):
         basic.discount_rate = 10
         basic.total = get_total(basic)
         basic.discounted_total = get_discounted_total(basic)
+
+        #create an array of the packages
+        pkgs = [basic, bronze, silver, gold, platinum]
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
