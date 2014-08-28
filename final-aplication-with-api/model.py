09a62c4c8626007144ee44d42540feb5dd3091ae
ericgrogers <ericgrogers@fullsail.edu>
2014-08-27 19:31:15 -0500

    added model and view templates

---
+++ final-aplication-with-api/model.py
@@ -0,0 +1,8 @@
+"""
+Eric Rogers
+08/25/14
+Design Patterns for Web Programming
+Final Project: Application with API
+API: http://www.omdbapi.com/
+"""
+
---
+++ final-aplication-with-api/view.py
@@ -0,0 +1,7 @@
+"""
+Eric Rogers
+08/25/14
+Design Patterns for Web Programming
+Final Project: Application with API
+API: http://www.omdbapi.com/
+"""
