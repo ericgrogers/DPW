67711f5e984775de262f7a244c7ed2f779a25a00
ericgrogers <ericgrogers@fullsail.edu>
2014-08-27 21:52:14 -0500

    creating inputs and initial page load in the main handler

--- final-aplication-with-api/main.py
+++ final-aplication-with-api/main.py
@@ -8,11 +8,17 @@ API: http://www.omdbapi.com/
 
 
 import webapp2
+from view import MovieView, PageFormView
+from model import MovieModel
 
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        pass
+        p = PageFormView()  # create an instance of PageFormView
+
+        #input format is 'type', 'name/value', 'placeholder'
+        p.inputs = [['text', 'search', 'Find a Movie'], ['submit', 'Search']]  # create the form inputs
+        self.response.write(p.compile_view())  # compile the initial page load
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
