17ec7b798f4313c4072bb46f67695c98a710b30f
ericgrogers <ericgrogers@fullsail.edu>
2014-08-27 19:35:51 -0500

    starting the MovieModel class

--- final-aplication-with-api/model.py
+++ final-aplication-with-api/model.py
@@ -6,3 +6,16 @@ Final Project: Application with API
 API: http://www.omdbapi.com/
 """
 
+# import the necessary libraries
+import urllib2
+import json
+
+
+class MovieModel(object):
+    """This model handles fetching, parsing, and sorting the data from the API."""
+
+    def __init__(self):
+        self.__url = "http://www.omdbapi.com/"  # setting the base url for the API
+        self.__search = ''  # used to store search values
+        self.__id = ''  # holds the movie id
+        self.__jdoc = ''  # holds the returned json
